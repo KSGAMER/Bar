6680075d8ac7a4278e93d4e6d0d63f689538966e
muert <muert@DESKTOP-EPE2IT1>
2019-01-24 02:53:16 -0600

    AddUser Terminado

--- src/Vista/AddUser.java
+++ src/Vista/AddUser.java
@@ -5,6 +5,11 @@
  */
 package Vista;
 
+import Controlador.Controlador;
+import java.awt.Dimension;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  *
  * @author muert
@@ -16,7 +21,24 @@ public class AddUser extends javax.swing.JInternalFrame {
      */
     public AddUser() {
         initComponents();
+//        SinBarra();
+        Limpiar();
+        
     }
+    Controlador contro = new Controlador();
+     void SinBarra(){
+        ((javax.swing.plaf.basic.BasicInternalFrameUI) 
+      this.getUI()).setNorthPane(null);
+         
+    }
+     void Limpiar(){
+         txtNombre.setText("");
+         txtApellidos.setText("");
+         txtUsuario.setText("");
+         JPContra.setText("");
+         CbPerfil.setSelectedIndex(1);
+         JDFecha.setDate(new Date(1997,11,23));
+     }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -36,13 +58,13 @@ public class AddUser extends javax.swing.JInternalFrame {
         txtApellidos = new javax.swing.JTextField();
         txtUsuario = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
-        txtPerfil = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
         JDFecha = new com.toedter.calendar.JDateChooser();
         jLabel7 = new javax.swing.JLabel();
         JPContra = new javax.swing.JPasswordField();
         BtnGuardar = new javax.swing.JLabel();
         BtnCancelar1 = new javax.swing.JLabel();
+        CbPerfil = new javax.swing.JComboBox<>();
 
         setClosable(true);
         setIconifiable(true);
@@ -69,8 +91,6 @@ public class AddUser extends javax.swing.JInternalFrame {
 
         jLabel5.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel5.setText("Perfil: ");
-
-        txtPerfil.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
 
         jLabel6.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel6.setText("Fecha: ");
@@ -104,6 +124,8 @@ public class AddUser extends javax.swing.JInternalFrame {
             }
         });
 
+        CbPerfil.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Administrados", "Normal", "X" }));
+
         javax.swing.GroupLayout JPrincipalLayout = new javax.swing.GroupLayout(JPrincipal);
         JPrincipal.setLayout(JPrincipalLayout);
         JPrincipalLayout.setHorizontalGroup(
@@ -131,9 +153,9 @@ public class AddUser extends javax.swing.JInternalFrame {
                             .addComponent(jLabel7))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(JPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(txtPerfil)
                             .addComponent(JDFecha, javax.swing.GroupLayout.DEFAULT_SIZE, 127, Short.MAX_VALUE)
-                            .addComponent(JPContra))))
+                            .addComponent(JPContra)
+                            .addComponent(CbPerfil, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                 .addContainerGap(104, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, JPrincipalLayout.createSequentialGroup()
                 .addGap(0, 0, Short.MAX_VALUE)
@@ -161,7 +183,7 @@ public class AddUser extends javax.swing.JInternalFrame {
                             .addComponent(jLabel2)
                             .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel5)
-                            .addComponent(txtPerfil, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(CbPerfil, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(27, 27, 27)
                         .addGroup(JPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(JPrincipalLayout.createSequentialGroup()
@@ -172,7 +194,7 @@ public class AddUser extends javax.swing.JInternalFrame {
                                 .addGap(34, 34, 34)
                                 .addComponent(jLabel4))
                             .addComponent(JDFecha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 48, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 52, Short.MAX_VALUE)
                 .addComponent(BtnGuardar, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
             .addGroup(JPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -197,17 +219,20 @@ public class AddUser extends javax.swing.JInternalFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void BtnGuardarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BtnGuardarMouseClicked
-       
+        SimpleDateFormat fec = new SimpleDateFormat("YYYY-MM-dd");
+        String Fecha2 = fec.format(JDFecha.getDate());
+        contro.AgregarUser(txtNombre.getText(), txtApellidos.getText(), txtUsuario.getText(),JPContra.getText(),Fecha2,CbPerfil.getSelectedItem().toString());
     }//GEN-LAST:event_BtnGuardarMouseClicked
 
     private void BtnCancelar1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BtnCancelar1MouseClicked
-        // TODO add your handling code here:
+       Limpiar();
     }//GEN-LAST:event_BtnCancelar1MouseClicked
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel BtnCancelar1;
     private javax.swing.JLabel BtnGuardar;
+    private javax.swing.JComboBox<String> CbPerfil;
     private com.toedter.calendar.JDateChooser JDFecha;
     private javax.swing.JPasswordField JPContra;
     private javax.swing.JPanel JPrincipal;
@@ -220,7 +245,6 @@ public class AddUser extends javax.swing.JInternalFrame {
     private javax.swing.JLabel jLabel7;
     private javax.swing.JTextField txtApellidos;
     private javax.swing.JTextField txtNombre;
-    private javax.swing.JTextField txtPerfil;
     private javax.swing.JTextField txtUsuario;
     // End of variables declaration//GEN-END:variables
 }
